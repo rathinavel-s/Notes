232357ba09787fca23841833eed44785a7a73a58
rathinavel-s <131685386+rathinavel-s@users.noreply.github.com>
2024-05-21 15:42:52 +0530

    Update Newpost.js

--- src/components/Newpost.js
+++ src/components/Newpost.js
@@ -10,7 +10,7 @@ const Newpost = ({postTitle,setPostTitle, handlesubmit,postBody,setpostBody}) =>
      <input type='text' required value={postTitle} onChange={(e)=> setPostTitle(e.target.value)}/>
 
       <label htmlFor='postTitle'>Notes:</label>
-      <textarea  style={{width:"765px",height:"50px"}} id='postBody' required value={postBody} onChange={(e)=>setpostBody(e.target.value)}/>
+      <textarea  id='postBody' required value={postBody} onChange={(e)=>setpostBody(e.target.value)}/>
       <button type='submit'>Submit</button>
      </form>
     </main>
